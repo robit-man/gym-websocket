8c5bf3c6c533e49d73594c4836434f57f16e0d70
Your Name <you@example.com>
2024-06-13 15:20:14 +0000

    Cuda Works + instructions

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Example Gymnasium RL Request Via Websockets
 
+update apt
+```sudo apg-get update && sudo apt-get swig```
+install reqs
 ```pip install -r requirements.txt```
 for cart
 ```python -m uvicorn main:app --reload```
--- cuda_test.py
+++ cuda_test.py
@@ -1,2 +1,4 @@
 import torch
-print(torch.cuda.is_available())
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA device count:", torch.cuda.device_count())
+print("CUDA device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU found")
